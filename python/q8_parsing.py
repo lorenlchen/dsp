b0316f54af1b015cd02e7a4ed8b9d88c831f889f
lorenlchen <lorenlchen@gmail.com>
2017-08-14 22:36:57 -0400

    Added code for q8 parsing

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -4,3 +4,20 @@
 # against opponents, and had 36 goals scored against them). Write a program to read the file, 
 # then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
 
+import csv
+
+with open('football.csv') as football:
+    football_reader = csv.DictReader(football)
+    differentials = []
+    for row in football_reader:
+        team_name = row['Team']
+        abs_diff = abs(int(row['Goals']) - int(row['Goals Allowed']))
+        diff = int(row['Goals']) - int(row['Goals Allowed'])
+        diff_tuple = (team_name, abs_diff, diff)
+        differentials.append(diff_tuple)
+
+sorted_absolute = sorted(differentials, key=lambda diff: diff[1])
+sorted_overall = sorted(differentials, key=lambda diff: diff[2])
+
+print(sorted_absolute[0][0] + " had the lowest absolute difference between 'for' and 'against' goals with " + str(sorted_absolute[0][1]))
+print(sorted_overall[0][0] + " had the worst goal differential with " + str(sorted_overall[0][2]))
