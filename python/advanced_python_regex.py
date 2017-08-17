578615189b6be126695f6ddeac7ba09c46b6ad4e
lorenlchen <lorenlchen@gmail.com>
2017-08-17 15:06:12 -0400

    Added regex code

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,1 +1,14 @@
-PLACE YOUR CODE HERE
+import re
+import csv
+
+with open('faculty.csv') as faculty:
+    upenn_faculty = csv.DictReader(faculty)
+    degrees = set()
+
+    for prof in upenn_faculty:
+        if prof[' degree'] != '0':
+            prof_degrees = prof[' degree'].split()
+            for item in prof_degrees:
+                degrees.add(re.sub(r'\W+','',item))
+
+print(degrees)
