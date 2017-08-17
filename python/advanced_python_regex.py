3b7fcaae84b87c7a64f46929d24e1511707cb2c3
lorenlchen <lorenlchen@gmail.com>
2017-08-17 15:45:38 -0400

    Added code for all q1 problems

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,11 +1,10 @@
 import re
 import csv
 
-with open('faculty.csv') as faculty:
-    upenn_faculty = csv.DictReader(faculty)
-    degrees = {}
 
-    for prof in upenn_faculty:
+def find_degrees(faculty_list):
+    degrees = {}
+    for prof in faculty_list:
         if prof[' degree'] != '0':
             prof_degrees = prof[' degree'].split()
             for item in prof_degrees:
@@ -14,5 +13,38 @@ with open('faculty.csv') as faculty:
                     degrees[degree] = degrees[degree] + 1
                 else:
                     degrees[degree] = 1
+    return(degrees)
 
-print(degrees)
+def find_titles(faculty_list):
+    titles = {}
+    for prof in faculty_list:
+        title = prof[' title'][:-17]
+        if title in titles:
+            titles[title] = titles[title] + 1
+        else:
+            titles[title] = 1
+    return(titles)
+
+def find_emails(faculty_list):
+    emails = []
+    for prof in faculty_list:
+        email = prof[' email']
+        emails.append(email)
+    return(emails)
+
+def find_domains(faculty_list):
+    domains = set()
+    for prof in faculty_list:
+        at_pos = prof[' email'].find('@')
+        domain = prof[' email'][(at_pos+1):]
+        domains.add(domain)
+    return list(domains)
+
+with open('faculty.csv') as faculty:
+    data = list(csv.DictReader(faculty))
+    upenn_faculty = [dict(x) for x in data]
+
+print('Degrees and their distribution: ' + str(find_degrees(upenn_faculty)))
+print('Titles and their distribution: ' + str(find_titles(upenn_faculty)))
+print('List of emails: ' + str(find_emails(upenn_faculty)))
+print('List of domains: ' + str(find_domains(upenn_faculty)))
