52e51f0a283494a46cc4f23d210eec7303ad1c27
lorenlchen <lorenlchen@gmail.com>
2017-08-17 15:10:12 -0400

    Misread instructions

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -3,12 +3,16 @@ import csv
 
 with open('faculty.csv') as faculty:
     upenn_faculty = csv.DictReader(faculty)
-    degrees = set()
+    degrees = {}
 
     for prof in upenn_faculty:
         if prof[' degree'] != '0':
             prof_degrees = prof[' degree'].split()
             for item in prof_degrees:
-                degrees.add(re.sub(r'\W+','',item))
+                degree = re.sub(r'\W+','',item)
+                if degree in degrees:
+                    degrees[degree] = degrees[degree] + 1
+                else:
+                    degrees[degree] = 1
 
 print(degrees)
