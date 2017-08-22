14a95893006d56f494224c065a3ef9f0aa66bc13
lorenlchen <lorenlchen@gmail.com>
2017-08-22 13:51:53 -0400

    Added code and file for CSV

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,20 @@
-PLACE YOUR CODE HERE
+import csv
+
+#From Q1
+def find_emails(faculty_list):
+    emails = []
+    for prof in faculty_list:
+        email = prof[' email']
+        emails.append(email)
+    return(emails)
+
+with open('faculty.csv') as faculty:
+    data = list(csv.DictReader(faculty))
+    upenn_faculty = [dict(x) for x in data]
+
+email_list = find_emails(upenn_faculty)
+
+with open('emails.csv', 'w', newline='') as csvfile:
+    emails = csv.writer(csvfile, delimiter=' ')
+    for email in email_list:
+        emails.writerow(email)
